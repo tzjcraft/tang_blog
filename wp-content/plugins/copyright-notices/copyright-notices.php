6ef87dddb1e74e84ce825e07ccebabcbf1993a9d
tzjcraft <tangzj@shinetechchina.com>
2013-08-12 15:01:42 +0800

    push to github

---
+++ wp-content/plugins/copyright-notices/copyright-notices.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Copyright Notices
+Plugin URIL http://www.xxxxx.com
+Description: A plugin that allows the user to set Copyright text in the theme and ontrol it from WordPress Admin.
+Author: Runby97
+Version: 1.0
+Author URI: http://www.xxxxx.com
+*/
