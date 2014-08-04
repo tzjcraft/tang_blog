660a97d49a7b94e3f5d24ee661d3c2f07fdb713e
Zugor Tang <tangzhujian@163.com>
2014-08-04 17:22:42 +0800

    Themes - first-tang

--- wp-content/themes/first-tang/index.php
+++ wp-content/themes/first-tang/index.php
@@ -5,5 +5,6 @@
     <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
     <p><?php the_excerpt(); ?></p>
 
-<?php endwhile; ?>
-<?php get_footer(); ?>
+        <?php endwhile; ?>
+    <?php wp_list_categories() ?>
+    <?php get_footer(); ?>
--- wp-content/themes/first-tang/single.php
+++ wp-content/themes/first-tang/single.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 <?php the_post() ?>
+<p><?php the_tags(); ?></p>
 <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
 <p><?php the_excerpt(); ?></p>
 
